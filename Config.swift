0602b990c0339d5b09ea2a8ac650fb635c319e09
Andres Altamirano <andres.altamirano.m@gmail.com>
2018-05-15 17:51:58 -0500

    create file to manage game configurations

---
+++ Config.swift
@@ -0,0 +1,17 @@
+//
+//  Config.swift
+//  Infinite Hoops
+//
+//  Created by Andres Altamirano on 5/15/18.
+//  Copyright © 2018 AndresAltamirano. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+final class Config {
+    static let defaultFont = "AmericanTypewriter-Bold"
+    static let defaultFontColor = SKColor.black
+    
+    private init() {}
+}
